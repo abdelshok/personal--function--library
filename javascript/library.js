cef8e9f1595bf0834832496d9473d6b7b9ccc807
abdelshok <aas698@nyu.edu>
2020-12-23 11:06:54 -0500

    struct(general): create folder structure for html, js, css, react, etc. libraries

---
+++ javascript/library.js
@@ -0,0 +1,9 @@
+
+/**
+ * 
+ * This file will start off as a single file and as the project and library progresses will become subdivided into different files
+ * that group functions of the same type. E.g. functions that solely focus on animations, functions that focus on the sequence of how 
+ * animations are done, etc.
+ * 
+ * 
+ */
