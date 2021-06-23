6cf4695d0578f45dc6b5b67f9f5603a89d03b225
abdelshok <aas698@nyu.edu>
2021-06-22 23:31:47 -0400

    feat(JS): add email validation and marquee related HTML code

---
+++ javascript/email.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ *
+ * Email Related Helper Functions.
+ * 
+ * e.g. Simple / complicated email validation, etc. 
+ *
+ * 
+ */
+
+
+/**
+ * 
+ * Simple email validation for strings such as 'anystring@anystring.anystring'
+ * 
+ * @param {string} email Do I really need to explain.
+ * @returns Boolean, specifying whether the email has the right pattern
+ */
+function validateEmail(email) 
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+} 
