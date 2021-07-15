3f96e43fc1e3ded1348ec43e346ff5815b1d48be
abdelshok <aas698@nyu.edu>
2021-07-15 00:02:21 -0400

    refactor(JS): create boilerplate for @detectBrowser function

--- javascript/browsers.js
+++ javascript/browsers.js
@@ -56,3 +56,42 @@ output += 'isEdge: ' + isEdge + '<br>';
 output += 'isEdgeChromium: ' + isEdgeChromium + '<br>';
 output += 'isBlink: ' + isBlink + '<br>';
 document.body.innerHTML = output;
+
+
+// Copy / Paste code below in new project
+
+let isOpera,
+    isSafari,
+    isFirefox,
+    isIE,
+    isEdge,
+    isChrome,
+    isEdgeChromium,
+    isBlink;
+
+function detectBrowser () {
+
+    isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    
+    // Firefox 1.0+
+    isFirefox = typeof InstallTrigger !== 'undefined';
+    
+    // Safari 3.0+ "[object HTMLElementConstructor]" 
+    isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+    
+    // Internet Explorer 6-11
+    isIE = /*@cc_on!@*/false || !!document.documentMode;
+    
+    // Edge 20+
+    isEdge = !isIE && !!window.StyleMedia;
+    
+    // Chrome 1 - 79
+    isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+    
+    // Edge (based on chromium) detection
+    isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+    
+    // Blink engine detection
+    isBlink = (isChrome || isOpera) && !!window.CSS;
+
+}
